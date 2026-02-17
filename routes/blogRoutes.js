1967e963da5e95e0cfdb29345f65df2dd06c4550
Vaishnavi Manikeri <vaishnavimanikeri@gmail.com>
2026-02-17 12:32:42 +0530

    updated rojtes

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const auth = require("../middleware/auth");
 const ctrl = require("../controllers/blogController");
 
-/* PUBLIC */
-router.get("/", ctrl.getPublishedBlogs);
-router.get("/:slug", ctrl.getSingleBlog);
-
-/* ADMIN */
+/* ADMIN FIRST */
 router.post("/", auth, ctrl.createBlog);
 router.get("/admin/all", auth, ctrl.getAllBlogs);
 router.put("/:id", auth, ctrl.updateBlog);
 router.delete("/:id", auth, ctrl.deleteBlog);
+
+/* PUBLIC LAST */
+router.get("/", ctrl.getPublishedBlogs);
+router.get("/:slug", ctrl.getSingleBlog);
 
 module.exports = router;
